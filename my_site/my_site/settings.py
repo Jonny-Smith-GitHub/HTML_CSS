befcb6d0d0ff0e45b6790b664aea06a4189a2b3d
Jonny-Smith-GitHub <qiling90@gmail.com>
2019-07-22 16:21:51 +0800

    practice1

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'fqa2t3cbzzfwh=e*x@exjgbz-8ndrb%rtoppskvc+oky=7u#pv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -35,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MyBlog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.contrib import admin
 from django.urls import path
 
+from MyBlog import views
+from MyBlog.views import archive
+
+
+admin.autodiscover()
+
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('MyBlog/templates/', views.archive),
 ]
